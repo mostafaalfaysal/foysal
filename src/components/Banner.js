1824b5d12426c07e96113dd8eec0cbe4ac697bc3
Mostafa Al Faysal <mostafaalfaysal@Mostafas-MacBook-Air.local>
2023-01-01 14:09:25 +0600

    updating

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,8 +58,8 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Mostafa AL Faysal,`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer"]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  <p>A Back-End Web Developer passionate about creating dynamic applications and experiences on the web. I am passionate about coding and solving problems through code, and I am excited to work alongside other amazing programmers and learn so much more!</p>
+                <button onClick={() => console.log('#connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
           </Col>
