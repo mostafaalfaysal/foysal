77e2f5acd8120ebe29037b00e55d274b784b7156
Mostafa Al Faysal <mostafaalfaysal@nj-69-69-69-178.sta.embarqhsd.net>
2023-01-01 21:49:29 +0600

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,7 @@ export const NavBar = () => {
 
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 50 ) {
         setScrolled(true);
       } else {
         setScrolled(false);
