81168ee9260ef885f3461fd466b7a4e17424d014
Mostafa Al Faysal <mostafaalfaysal@Mostafas-MacBook-Air.local>
2023-01-13 23:43:41 +0600

    fix links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,8 +51,8 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/mostafaalfaysal/"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/mostafaalfaysal"><img src={navIcon3} alt="" /></a>
                 <a href="https://www.facebook.com/mostafaalfaysal/"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/mostafaalfaysal/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
